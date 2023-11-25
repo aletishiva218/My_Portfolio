60f65900e7a13f3722c52f76721c001fd2c988ef
Shiva Aleti <aletishiva218@gmail.com>
2023-11-25 14:00:43 +0530

    Changed host site

--- index.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const app=express()
-const path=require("path")
-const port=process.env.port || 3000;
-const webPath=path.join(__dirname,"website")
-app.use(express.static(webPath))
-
-app.listen(port)
